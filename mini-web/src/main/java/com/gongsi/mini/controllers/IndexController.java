f207f787c477685f72ebe56d251459d8aa4ed847
吴宇 <wuyu@xkeshi.com>
2018-07-22 21:51:45 +0800

    增加错误页面

---
+++ mini-web/src/main/java/com/gongsi/mini/controllers/IndexController.java
@@ -0,0 +1,39 @@
+package com.gongsi.mini.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by 吴宇 on 2018-05-26.
+ */
+@RestController
+public class IndexController {
+    /**
+     * 默认首页
+     */
+    @RequestMapping(value = "/")
+    public String add(){
+        return "I konw you are missing yourself !";
+    }
+
+    /**
+     * 错误页面
+     */
+    @RequestMapping(value = "/error/*")
+    public String error(HttpServletResponse response){
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Cache-Control", "no-cache, must-revalidate");
+        return "I konw you are missing yourself !\n" +
+                "别乱搞 ！";
+    }
+
+    /** 测试页面 */
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
+    public String test(){
+        return "Welcome to this page !";
+    }
+}
--- mini-web/src/main/java/com/gongsi/mini/core/MiniInterceptor.java
+++ mini-web/src/main/java/com/gongsi/mini/core/MiniInterceptor.java
@@ -7,14 +7,23 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by 吴宇 on 2018-05-29.
  */
 @Slf4j
 public class MiniInterceptor implements HandlerInterceptor {
+    /** 错误页面 */
+    private List<Integer> errorCodeList = Arrays.asList(404, 403, 500, 501);
+
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        if (errorCodeList.contains(httpServletResponse.getStatus())){
+            httpServletResponse.sendRedirect("/error/" + httpServletResponse.getStatus());
+            return false;
+        }
         MiniContext context = MiniContext.getContext();
         if (!context.isInited()){
             context.init(httpServletRequest);
