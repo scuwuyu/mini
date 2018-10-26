2f47710e4fad5f1a467130c2c7845d5b5e7b16e1
吴宇 <wuyu@xkeshi.com>
2018-10-26 21:44:03 +0800

    修改请求方法

--- mini-business/src/main/java/com/gongsi/mini/services/http/WechatAuthService.java
+++ mini-business/src/main/java/com/gongsi/mini/services/http/WechatAuthService.java
@@ -1,6 +1,8 @@
 package com.gongsi.mini.services.http;
 
+import com.gongsi.mini.entities.AppToken;
 import com.gongsi.mini.services.http.base.BaseService;
+import com.gongsi.mini.services.http.base.TokenResult;
 import com.gongsi.mini.vo.auth.AuthVO;
 
 /**
@@ -9,10 +11,20 @@ import com.gongsi.mini.vo.auth.AuthVO;
 public class WechatAuthService extends BaseService {
     /** jscode 换取 openId 和 session_key*/
     private static final String AUTH_URL = "https://api.weixin.qq.com/sns/jscode2session?" +
-            "appid=wxd03b7dc48f53984b&secret=&js_code=JSCODE&grant_type=authorization_code";
+            "appid=APPID&secret=APPSECRET&js_code=JSCODE&grant_type=authorization_code";
+    /** 刷新tokenurl*/
+    private static final String TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential" +
+            "&appid=APPID&secret=APPSECRET";
 
-    public static WechatAuthResult authByJSCode(AuthVO authVO){
-        String requestUrl = AUTH_URL.replace("JSCODE",authVO.getCode());
+
+    public static WechatAuthResult authByJSCode(AuthVO authVO, AppToken appToken){
+        String requestUrl = AUTH_URL.replace("JSCODE",authVO.getCode())
+                .replace("APPID",appToken.getAppId()).replace("APPSECRET",appToken.getSecret());
         return exePostJson(requestUrl,null, WechatAuthResult.class);
+    }
+
+    public static TokenResult getToken(String appId,String secret){
+        String requestUrl = TOKEN_URL.replace("APPID",appId).replace("APPSECRET",secret);
+        return exeGetJson(requestUrl,TokenResult.class);
     }
 }
