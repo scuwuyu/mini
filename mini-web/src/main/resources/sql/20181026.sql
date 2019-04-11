b24b085ba5264ceb8294b1568a807775d2f4a8ac
wuyu <wuyu@dianwoba.com>
2019-04-11 15:50:29 +0800

    增加generator

--- mini-web/src/main/resources/sql/20181026.sql
+++ mini-web/src/main/resources/sql/20181026.sql
@@ -11,6 +11,17 @@ CREATE TABLE `app_token` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='app_token';
 
+--  stock_code
+CREATE TABLE `stock_code` (
+  `id` BIGINT(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `code` varchar(16) NOT NULL COMMENT 'code',
+  `active` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否有效',
+
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `modify_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stock_code';
+
 
 
 
